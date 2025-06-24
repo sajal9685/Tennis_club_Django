cc95cbd4f57f5d9fb1f9759d5f17e382a5ef5507
Sajal Chaturvedi <sajalchaturvedi8@gmail.com>
2025-06-24 22:06:06 +0530

    templates

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mg6e9+o_o2@&%5lke=2(r)y0gnf^p#)h9-udbn88+8xrzmuaby'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
